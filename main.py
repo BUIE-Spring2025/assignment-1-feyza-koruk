7de705ebf8c8e4ae120bde4000d5369fda70b539
feyza-koruk <feyza.koruk@boun.edu.tr>
2025-03-08 20:59:54 +0300

    updated main and tested int_to_roman

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ def int_to_roman(num):
     """
     Convert an integer to a Roman numeral.
 
+    constraints: num <= 3999
+
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
     
     ref_list = [ (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
-            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I") ]
+                 (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+                 (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I") ]
         
     roman = []
         
@@ -20,4 +22,4 @@ def int_to_roman(num):
 
             num -= value
                 
-    return " ".join(roman)
+    return ("".join(roman))
